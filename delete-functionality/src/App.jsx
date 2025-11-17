8d404a6ac688b52575e53f092ad4815da5ee13a4
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2025-11-17 20:54:17 +0530

    form validation added

--- delete-functionality/src/App.jsx
+++ delete-functionality/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import button from "react-bootstrap/Button";
-import table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 
 function App() {
   const [name, setName] = useState("");
@@ -11,19 +11,43 @@ function App() {
   const [number, setNumber] = useState("");
   const [submitData, setSubmitData] = useState([]);
   const [id, setId] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [numberError, setNumberError] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const isValidateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
   const handleName = (e) => {
-    setName(e.target.value);
+    const value = e.target.value;
+    setName(value);
+    if (value.length < 3) {
+      setNameError("Name must be at least 3 characters long");
+    } else if (value.length > 25) {
+      setNameError("Name must not have more then 25 characters");
+    } else {
+      setNameError("");
+    }
   };
   const handleEmail = (e) => {
     setEmail(e.target.value);
+    let value = e.target.value;
+    setEmailError(isValidateEmail(value) ? "" : "Invalid Email Address");
   };
   const handleNumber = (e) => {
     setNumber(e.target.value);
+    let value = e.target.value;
+    if (value.length < 10) {
+      setNumberError("number must have 10 digits");
+    } else if (value.length > 10) {
+      setNumberError("number must have 10 digits");
+    } else {
+      setNumberError("");
+    }
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
     // If no ID => add new
     if (!id) {
       setSubmitData([...submitData, { id: Date.now(), name, email, number }]);
@@ -36,106 +60,108 @@ function App() {
       setSubmitData(updated);
       setId(""); // exit edit mode
     }
-
     // Reset fields
     setName("");
     setEmail("");
     setNumber("");
+    if (!isValidateEmail(email)) {
+      setEmailError("Invalid Email Address");
+    } else {
+      setEmail("");
+      setEmailError("");
+    }
   };
-
   const handleDelete = (id) => {
     const dt = submitData.filter((res) => res.id !== id);
     setSubmitData(dt);
   };
   const handleEdit = (id) => {
     const dtt = submitData.filter((item) => item.id === id);
-      if (dtt.length > 0) {
-        setName(dtt[0].name);
-        setEmail(dtt[0].email);
-        setNumber(dtt[0].number);
-        setId(dtt[0].id);
-      }
-    };
-
-    return (
-      <>
-        <div
-          className="container"
-          style={{
-            height: "100vh",
-            width: "70vh",
-            backgroundColor: "#6c757d",
-            padding: "10px",
-            borderRadius: "10px",
-          }}
-        >
-          <h1
-            style={{ background: "", marginTop: "10px", borderRadius: "5px" }}
-          >
-            User Form
-          </h1>
-          <form action="submit">
-            <div>
-              <div
-                className="form-control"
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "10px", // spacing between rows
-                  marginTop: "5px",
-                  padding: "10px",
-                  background: "white",
-                  borderRadius: "10px",
-                }}
-              >
-                <label>NAME:</label>
-                <input
-                  type="text"
-                  placeholder="enter your name"
-                  value={name}
-                  onChange={handleName}
-                  style={{ marginTop: "10px" }}
-                />
-                <label for="email">EMAIL:</label>
-                <input
-                  type="email"
-                  placeholder="enter your email"
-                  value={email}
-                  onChange={handleEmail}
-                />
-                <label for="number">NUMBER:</label>
-                <input
-                  type="number"
-                  placeholder="enter your number"
-                  value={number}
-                  onChange={handleNumber}
-                />
-                <button onClick={handleSubmit} className="btn btn-success mt-3">
-                  submit
-                </button>
-              </div>
-            </div>
-          </form>
-          <br />
-          <div
-            className="table-responsive"
-            style={{ maxHeight: "50vh", overflowY: "auto", overflowX: "auto" }} //For scroll bar
-          >
-            <table
-              className="table table-striped table-bordered table-hover "
-              style={{ backgroundColor: "white", borderRadius: "10px" }}
+    if (dtt.length > 0) {
+      setName(dtt[0].name);
+      setEmail(dtt[0].email);
+      setNumber(dtt[0].number);
+      setId(dtt[0].id);
+    }
+  };
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          height: "100vh",
+          width: "70vh",
+          backgroundColor: "#6c757d",
+          padding: "10px",
+          borderRadius: "10px",
+        }}
+      >
+        <h1 style={{ background: "", marginTop: "10px", borderRadius: "5px" }}>
+          User Form
+        </h1>
+        <form action="submit" onSubmit={handleSubmit}>
+          <div>
+            <div
+              className="form-control"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px", // spacing between rows
+                marginTop: "5px",
+                padding: "10px",
+                background: "white",
+                borderRadius: "10px",
+              }}
             >
-              <thead>
-                <tr>
-                  <th className="ml-4">NAME</th>
-                  <th>EMAIL</th>
-                  <th>NUMBER</th>
-                  <th>Delete</th>
-                  <th>Edit</th>
-                </tr>
-              </thead>
+              <label>NAME:</label>
+              <input
+                type="text"
+                placeholder="enter your name"
+                value={name}
+                onChange={handleName}
+                style={{ marginTop: "10px" }}
+              />
+              {nameError && <p style={{ color: "red" }}>{nameError}</p>}
+              <label for="email">EMAIL:</label>
+              <input
+                type="email"
+                placeholder="enter your email"
+                value={email}
+                onChange={handleEmail}
+              />
+              {emailError && <p style={{ color: "red" }}>{emailError}</p>}
+              <label for="number">NUMBER:</label>
+              <input
+                type="number"
+                placeholder="enter your number"
+                value={number}
+                onChange={handleNumber}
+              />
+              {numberError && <p style={{ color: "red" }}>{numberError}</p>}
+              <button className="btn btn-success mt-3">submit</button>
+            </div>
+          </div>
+        </form>
+        <br />
+        <div
+          className="table-responsive"
+          style={{ maxHeight: "30vh", overflowY: "auto", overflowX: "auto" }} //For scroll bar
+        >
+          <table
+            className="table table-striped table-bordered table-hover "
+            style={{ backgroundColor: "white", borderRadius: "10px" }}
+          >
+            <thead>
+              <tr>
+                <th className="ml-4">NAME</th>
+                <th>EMAIL</th>
+                <th>NUMBER</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
               {submitData.map((res) => (
-                <tr>
+                <tr key={res.id}>
                   <td>{res.name}</td>
                   <td>{res.email}</td>
                   <td>{res.number}</td>
@@ -157,12 +183,12 @@ function App() {
                   </td>
                 </tr>
               ))}
-            </table>
-          </div>
+            </tbody>
+          </table>
         </div>
-      </>
-    );
-  };
-
+      </div>
+    </>
+  );
+}
 
 export default App;
